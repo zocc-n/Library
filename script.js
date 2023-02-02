26854150ef9eb49699bdca15e787751b230c2e8f
zocc-n <znanic@yahoo.com>
2023-02-02 01:26:47 +0100

    small changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(event){
