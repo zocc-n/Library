e6579aac1dbb76f28e1f4157dccc9ddac8ae5c22
zocc-n <znanic@yahoo.com>
2023-02-12 12:31:06 +0100

    Added card creating function. Removed testing part from HTML.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let myLibrary = [];
 
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
+const form = document.querySelector('form');
+const main = document.querySelector('main');
+
+form.addEventListener('submit', addBookToLibrary);
+
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -9,16 +19,47 @@ function Book(title, author, pages, read){
 
 function addBookToLibrary(event){
     event.preventDefault();
-    console.log(title.value);
     let newBook = new Book(title.value, author.value, pages.value, read.value);
     myLibrary.push(newBook);
     form.reset();
+    setTimeout(createCard(), 3000);
 }
 
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const read = document.querySelector('#read');
-const form = document.querySelector('form');
-
-form.addEventListener('submit', addBookToLibrary);
+function createCard(){
+    let i = myLibrary.length - 1;
+    let readStatus = '';
+    if(myLibrary[i].read === 'yes'){   //ovo ispravit kasnije
+        readStatus = 'checked';
+    }
+    main.innerHTML += `
+    <div class="card">
+                <div class="remove-container">
+                    <div>✖</div>
+                </div>
+                <div class="card-inner-container">
+                    <div class="name-container">
+                        <p class="bookName">${myLibrary[i].title}</p>
+                    </div>
+                    <div class="info-container">
+                        <div class="author-container">
+                            <div><b>Author:</b></div>
+                            <div>${myLibrary[i].author}</div>
+                        </div>
+                        <div class="pages-container">
+                            <div><b>Pages:</b></div>
+                            <div>${myLibrary[i].pages}</div>
+                        </div>
+                        <div class="read-container">
+                            <div>
+                                <label for="read"><b>Read:</b></label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="read" name="read" ${readStatus}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    <div class="switch-container">
+                </div>
+            </div>`
+}
